6cfd14ac92469f45d4c26a64fdafb04446965abc
João Moutinho <123136954+JoaoGabrielMoutinho@users.noreply.github.com>
2025-11-18 22:09:40 -0300

    Atualização imagens barbearias

--- src/interface/Cliente/barbearias.js
+++ src/interface/Cliente/barbearias.js
@@ -7,49 +7,49 @@ document.addEventListener("DOMContentLoaded", () => {
       endereco: "Rua das Flores, 123 - Centro",
       avaliacao: 4.8,
       totalAvaliacoes: 127,
-      imagem: "img/barbearia1.jpg"
+      imagem: "../img/barbearia1.jpg"
     },
     {
       nome: "Barbearia Clássica",
       endereco: "Av. Brasil, 200 - Jardim",
       avaliacao: 4.6,
       totalAvaliacoes: 89,
-      imagem: "img/barbearia2.jpg",
+      imagem: "../img/barbearia2.jpg",
     },
     {
       nome: "Old School Barber",
       endereco: "Rua XV de Novembro, 45 - Centro",
       avaliacao: 4.9,
       totalAvaliacoes: 203,
-      imagem: "img/barbearia3.jpg",
+      imagem: "../img/barbearia3.jpg",
     },
     {
       nome: "Barbearia Premium",
       endereco: "Av. Independência, 890 - Bela Vista",
       avaliacao: 4.7,
       totalAvaliacoes: 156,
-      imagem: "img/barbearia4.jpg",
+      imagem: "../img/barbearia4.jpg",
     },
     {
       nome: "Barbearia Reino Dos Cortes",
       endereco: "Rua Projetada, 999 - Centro",
       avaliacao: 4.5,
       totalAvaliacoes: 42,
-      imagem: "img/barbearia5.jpg",
+      imagem: "../img/barbearia5.jpg",
     },
     {
       nome: "Barbearia Vinicin",
       endereco: "Rua Dona Ana, 137 - Amazonas",
       avaliacao: 4.5,
       totalAvaliacoes: 29,
-      imagem: "img/barbearia6.jpg",
+      imagem: "../img/barbearia6.jpg",
     },
     {
       nome: "Barbearia Do Zé",
       endereco: "Rua Tupi, 34 - Industrial",
       avaliacao: 4.8,
       totalAvaliacoes: 33,
-      imagem: "img/barbearia7.jpg",
+      imagem: "../img/barbearia7.jpg",
     },
   ];
 
