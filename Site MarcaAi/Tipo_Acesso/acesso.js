9e5d2b0d1d2de5cb7ffa0b75d23f1a79d4446af8
talisson04 <talissonvsilva@gmail.com>
2025-11-28 18:38:51 -0300

    Update acesso.js

--- Site MarcaAi/Tipo_Acesso/acesso.js
+++ Site MarcaAi/Tipo_Acesso/acesso.js
@@ -1,24 +1,20 @@
-// Ativa ícones Lucide
 lucide.createIcons();
 
-// Verifica de onde o usuário veio (iniciar ou cadastro)
 const params = new URLSearchParams(window.location.search);
-const acao = params.get("acao"); // retorna 'iniciar' ou 'cadastro'
+const acao = params.get("acao"); 
 
-// Quando clicar em CLIENTE
 document.getElementById("cliente").addEventListener("click", () => {
   if (acao === "cadastro") {
-    window.location.href = "../Cliente/cadastro.html"; // tela de cadastro
+    window.location.href = "../Cliente/cadastro.html";
   } else {
-    window.location.href = "../Cliente/login.html"; // tela de login
+    window.location.href = "../Cliente/login.html"; 
   }
 });
 
-// Quando clicar em BARBEARIA
 document.getElementById("barbearia").addEventListener("click", () => {
   if (acao === "cadastro") {
-    window.location.href = "../Barbearia/cadastro.html"; // tela de cadastro
+    window.location.href = "../Barbearia/cadastro.html"; 
   } else {
-    window.location.href = "../Barbearia/login.html"; // tela de login
+    window.location.href = "../Barbearia/login.html"; 
   }
 });
