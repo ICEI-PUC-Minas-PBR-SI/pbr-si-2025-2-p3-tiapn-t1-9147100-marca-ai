a8b2bdf0e4027795b738ee48cb577a7b3540cd7a
João Moutinho <123136954+JoaoGabrielMoutinho@users.noreply.github.com>
2025-11-28 22:47:05 -0300

    teste backend js

---
+++ src/backend/controllers/usuario.controller.js
@@ -0,0 +1,19 @@
+const Usuario = require('../models/usuario');
+
+exports.criarUsuario = async (req, res) => {
+  try {
+    const novoUsuario = await Usuario.create(req.body); // CREATE no MySQL
+    res.status(201).json(novoUsuario);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+exports.listarUsuarios = async (req, res) => {
+  try {
+    const usuarios = await Usuario.findAll(); // READ (SELECT *) no MySQL
+    res.status(200).json(usuarios);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
